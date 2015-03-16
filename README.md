85ed5afa4c5fa5a3b1ce52951b5334d8141d0a33
leilux <leilux.dev@gmail.com>
2015-03-16 15:01:14 +0800

    add arduino-remote-control.png

---
+++ README.md
@@ -0,0 +1,5 @@
+#arduino-remote-control
+
+Use the web to remotely control the robot
+
+![](https://raw.githubusercontent.com/leilux/arduino-remote-control/master/arduino-remote-control.png)
--- src/sketch.ino
+++ src/sketch.ino
@@ -15,73 +15,73 @@ int count=0;
 EthernetServer server(80);
 
 void setup() {
-//	SPI.begin();
-//	SPI.setBitOrder(MSBFIRST);
-//	SPI.setDataMode(SPI_MODE0);
+//    SPI.begin();
+//    SPI.setBitOrder(MSBFIRST);
+//    SPI.setDataMode(SPI_MODE0);
 
-//	pinMode(3, OUTPUT);
-//	digitalWrite(3, HIGH);
-	pinMode(led, OUTPUT);
+//    pinMode(3, OUTPUT);
+//    digitalWrite(3, HIGH);
+    pinMode(led, OUTPUT);
 
-	Ethernet.begin(mac, ip);
+    Ethernet.begin(mac, ip);
 
-	server.begin();
+    server.begin();
 }
 
 void loop() {
-	count = 0;
-	EthernetClient client = server.available();
-	if (client) {
-		boolean currentLineIsBlank = true;
-		while (client.connected()) {
-			if (client.available()) {
-				char c = client.read();
-				if (c=='\n' && currentLineIsBlank) {    
-					break;
-				}
-				request_header[count++] = c;
-				if (c=='\n') {
-					currentLineIsBlank = true;
-				}
-				else if (c!='\r') {
-					currentLineIsBlank = false;
-				}
-			}
-		}
+    count = 0;
+    EthernetClient client = server.available();
+    if (client) {
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                if (c=='\n' && currentLineIsBlank) {    
+                    break;
+                }
+                request_header[count++] = c;
+                if (c=='\n') {
+                    currentLineIsBlank = true;
+                }
+                else if (c!='\r') {
+                    currentLineIsBlank = false;
+                }
+            }
+        }
         request_header[count] = '\0';
         get_url_from_request(request_header, url);
         parse_url(url, path, cgi_args);
 
         if (strcmp(path, "")==0) {
-        	client.println("HTTP/1.1 200 ok");
-          	client.println("Server: arduino mege 2560");
-		  	client.println("Content-Type: text/html");
-		  	client.println();
-          	client.print(led_control);
+            client.println("HTTP/1.1 200 ok");
+            client.println("Server: arduino mege 2560");
+            client.println("Content-Type: text/html");
+            client.println();
+            client.print(led_control);
         }
         else if (strcmp(path,"on")==0) {
-        	led_on();
-        	client.println("HTTP/1.1 200 ok");
-			client.println("Content-Type: text/html");
-			client.println();
+            led_on();
+            client.println("HTTP/1.1 200 ok");
+            client.println("Content-Type: text/html");
+            client.println();
         }
         else if (strcmp(path, "off")==0) {
-        	led_off();
-          	client.println("HTTP/1.1 200 ok");
-		  	client.println("Content-Type: text/html");
-		  	client.println();
+            led_off();
+            client.println("HTTP/1.1 200 ok");
+            client.println("Content-Type: text/html");
+            client.println();
         }
-		else if (strcmp(path, "status")==0) {
-          	client.println("HTTP/1.1 200 ok");
-		  	client.println("Content-Type: application/json");
-		  	client.println();
-			client.print(digitalRead(led)==LOW ? "{\"status\":\"off\"}" : "{\"status\":\"on\"}");
-		}
-		else {}
+        else if (strcmp(path, "status")==0) {
+            client.println("HTTP/1.1 200 ok");
+            client.println("Content-Type: application/json");
+            client.println();
+            client.print(digitalRead(led)==LOW ? "{\"status\":\"off\"}" : "{\"status\":\"on\"}");
+        }
+        else {}
                 
-		delay(1);
-		client.stop();
-	} // /.if 
+        delay(1);
+        client.stop();
+    } // /.if 
 }
 
 void led_on() {
